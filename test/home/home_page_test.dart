da0709d398344cc9718db6ce550e3b973c00e3bc
Adby Santos <adby.santos15@outlook.com>
2023-01-13 14:00:20 -0300

    feat: updating example

--- test/home/home_page_test.dart
+++ test/home/home_page_test.dart
@@ -1,18 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:goldentest/home_page.dart';
-
 import 'package:golden_toolkit/golden_toolkit.dart';
+import 'package:goldentest/home_page.dart';
 
 void main() {
   testGoldens('Warning Color', (tester) async {
     await loadAppFonts();
+    final deviceDesktop = const Device(size: Size(1920, 1080), name: 'Full HD Desktop');
     final builder = DeviceBuilder()
       ..overrideDevicesForAllScenarios(devices: [
         Device.phone,
         Device.iphone11,
         Device.tabletPortrait,
         Device.tabletLandscape,
+        deviceDesktop,
       ])
       ..addScenario(
         name: 'No Text',
@@ -22,9 +23,7 @@ void main() {
         name: 'Password Hided',
         widget: const HomePage(),
         onCreate: (scenarioWidgetKey) async {
-          final txt = find.descendant(
-              of: find.byKey(scenarioWidgetKey),
-              matching: find.byKey(const Key('txtPassword')));
+          final txt = find.descendant(of: find.byKey(scenarioWidgetKey), matching: find.byKey(const Key('txtPassword')));
           expect(txt, findsWidgets);
           await tester.enterText(txt.first, '123');
         },
@@ -33,12 +32,9 @@ void main() {
         name: 'Password Showed',
         widget: const HomePage(),
         onCreate: (scenarioWidgetKey) async {
-          final txt = find.descendant(
-              of: find.byKey(scenarioWidgetKey),
-              matching: find.byKey(const Key('txtPassword')));
+          final txt = find.descendant(of: find.byKey(scenarioWidgetKey), matching: find.byKey(const Key('txtPassword')));
           expect(txt, findsWidgets);
-          final icon =
-              find.descendant(of: txt, matching: find.byType(IconButton));
+          final icon = find.descendant(of: txt, matching: find.byType(IconButton));
 
           expect(icon, findsOneWidget);
           await tester.enterText(txt.first, '123');
